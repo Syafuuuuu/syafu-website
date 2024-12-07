7f218953e5845e35de6dc6e01b8eb04714f05eae
Syafuuuuu <syafuedu@gmail.com>
2024-12-07 19:13:26 +0800

    Test absolute paths for images

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -3,47 +3,75 @@ import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 type ToolCategory = { name: string; icon: string };
 type Tools = { [category: string]: ToolCategory[] };
+
+import octave from "/assets/octave.png";
+import MATLAB from "/assets/MATLAB.png";
+import powerbi from "/assets/powerbi.png";
+import keras from "/assets/keras.png";
+import tf from "/assets/tf.png";
+import html from "/assets/html.png";
+import python from "/assets/python.png";
+
+import react from "/assets/react.png";
+import js from "/assets/js.png";
+import css from "/assets/css.png";
+import flutter from "/assets/flutter.png";
+import bootstrap from "/assets/bootstrap.png";
+import flask from "/assets/flask.png";
+import django from "/assets/django.png";
+import java from "/assets/java.png";
+import php from "/assets/php.png";
+import mysql from "/assets/mysql.png";
+import firebase from "/assets/firebase.png";
+import postgre from "/assets/postgre.png";
+import arduino from "/assets/arduino.png";
+import raspberry from "/assets/raspberry.png";
+import esp from "/assets/esp.png";
+import figma from "/assets/figma.png";
+import ai from "/assets/ai.png";
+import ps from "/assets/ps.png";
+
 const tools: Tools = {
   "Data Analytic & Ai": [
-    { name: "Octave", icon: "/assets/octave.png" },
-    { name: "MATLAB", icon: "/assets/MATLAB.png" },
-    { name: "Power BI", icon: "/assets/powerbi.png" },
-    { name: "Keras", icon: "/assets/keras.png" },
-    { name: "TensorFlow", icon: "/assets/tf.png" },
-    { name: "Orange", icon: "/assets/html.png" },
-    { name: "Python", icon: "/assets/python.png" },
+    { name: "Octave", icon: octave },
+    { name: "MATLAB", icon: MATLAB },
+    { name: "Power BI", icon: powerbi },
+    { name: "Keras", icon: keras },
+    { name: "TensorFlow", icon: tf },
+    { name: "Orange", icon: html },
+    { name: "Python", icon: python },
   ],
   Frontend: [
-    { name: "React", icon: "/assets/react.png" },
-    { name: "JavaScript", icon: "/assets/js.png" },
-    { name: "CSS", icon: "/assets/css.png" },
-    { name: "HTML", icon: "/assets/html.png" },
-    { name: "Flutter", icon: "/assets/flutter.png" },
-    { name: "Bootstrap", icon: "/assets/bootstrap.png" },
-    { name: "Flask", icon: "/assets/flask.png" },
-    { name: "Django", icon: "/assets/django.png" },
+    { name: "React", icon: react },
+    { name: "JavaScript", icon: js },
+    { name: "CSS", icon: css },
+    { name: "HTML", icon: html },
+    { name: "Flutter", icon: flutter },
+    { name: "Bootstrap", icon: bootstrap },
+    { name: "Flask", icon: flask },
+    { name: "Django", icon: django },
   ],
   Backend: [
-    { name: "JavaScript", icon: "/assets/js.png" },
-    { name: "Java", icon: "/assets/java.png" },
-    { name: "PHP", icon: "/assets/php.png" },
-    { name: "Python", icon: "/assets/python.png" },
-    { name: "MySQL", icon: "/assets/mysql.png" },
+    { name: "JavaScript", icon: js },
+    { name: "Java", icon: java },
+    { name: "PHP", icon: php },
+    { name: "Python", icon: python },
+    { name: "MySQL", icon: mysql },
   ],
   Database: [
-    { name: "MySQL", icon: "/assets/mysql.png" },
-    { name: "Firebase", icon: "/assets/firebase.png" },
-    { name: "PostgreSQL", icon: "/assets/postgre.png" },
+    { name: "MySQL", icon: mysql },
+    { name: "Firebase", icon: firebase },
+    { name: "PostgreSQL", icon: postgre },
   ],
   IOT: [
-    { name: "Arduino", icon: "/assets/arduino.png" },
-    { name: "Raspberry Pi", icon: "/assets/raspberry.png" },
-    { name: "ESP8266", icon: "/assets/esp.png" },
+    { name: "Arduino", icon: arduino },
+    { name: "Raspberry Pi", icon: raspberry },
+    { name: "ESP8266", icon: esp },
   ],
   "UI/UX Design": [
-    { name: "Figma", icon: "/assets/figma.png" },
-    { name: "Photoshop", icon: "/assets/ps.png" },
-    { name: "Illustrator", icon: "/assets/ai.png" },
+    { name: "Figma", icon: figma },
+    { name: "Photoshop", icon: ps },
+    { name: "Illustrator", icon: ai },
   ],
 };
 const Skills = () => {
