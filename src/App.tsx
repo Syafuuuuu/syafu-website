66a29726d89d2af5c0af5892663e38bf4f56568b
Syafuuuuu <syafuedu@gmail.com>
2024-12-07 11:20:53 +0800

    Added PDF Reader #2

--- src/App.tsx
+++ src/App.tsx
@@ -27,10 +27,6 @@ const App = () => (
     <div className="container-fluid p-0">
       <div className="background"></div>
       <Navbar />
-      {/* <header className="text-center py-5 section">
-        <h1 className="text-offwhite">Syafiq Azhari</h1>
-        <p>Computer Science Student | AI | Data Analytics | UI/UX Design</p>
-      </header> */}
       <main>
         <Routes>
           <Route path="/" element={<Hero />} />
